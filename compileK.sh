0772ed9e754f8d6c4bb9de79c7f8729129869067
Cassius Puodzius <puodzius@gmail.com>
2013-07-03 10:00:45 -0300

    Reset do Kernel para a versão padrão \n\n O número da versão mudou, para ficar igual à versão usada pelo professor. O arquivo de configuração usado foi o fornecido pelo professor

--- compileK.sh
+++ compileK.sh
@@ -1,3 +1,3 @@
-cd linux-2.6.34.14
+cd linux-2.6.39
 make ARCH=arm menuconfig
 make ARCH=arm CROSS_COMPILE=arm-none-eabi-
--- rootfs.sh
+++ rootfs.sh
@@ -3,6 +3,6 @@ cp -r bin/* boot
 cd boot
 find . -depth -print | cpio -o --format=newc > rootfs
 find . -depth -print
-mv rootfs ~/Public/Poli/3_MA/SO/projeto/linux-2.6.34.14/arch/arm/boot
+mv rootfs ~/Public/Poli/3_MA/SO/projeto/linux-2.6.39/arch/arm/boot
 cd ..
 rm -r boot
